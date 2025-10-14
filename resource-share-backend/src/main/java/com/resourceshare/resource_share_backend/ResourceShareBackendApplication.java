aa2096323e3978aaba0bab8c88ecd1eb76491b7e
wellinmart32 <wellinmart32@gmail.com>
2025-10-14 03:14:16 -0500

    Creación del backend

---
+++ resource-share-backend/src/main/java/com/resourceshare/resource_share_backend/ResourceShareBackendApplication.java
@@ -0,0 +1,13 @@
+package com.resourceshare.resource_share_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceShareBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourceShareBackendApplication.class, args);
+	}
+
+}
---
+++ resource-share-backend/src/test/java/com/resourceshare/resource_share_backend/ResourceShareBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resourceshare.resource_share_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceShareBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
